b6f56e5cb10ee47d93d8b41a05edd87df901019a
jayone <imdff0803@gmail.com>
2022-03-04 06:57:08 +0900

    statement - mapping dataclass

--- refactoring_2e/chap01_example/main.py
+++ refactoring_2e/chap01_example/main.py
@@ -1,48 +1,80 @@
+from dataclasses import dataclass
 import os
 import json
 
 
-def load_data() -> tuple[dict, list[dict]]:
+@dataclass
+class Play:
+    name: str
+    type: str
+
+
+@dataclass
+class Performance:
+    playID: str
+    audience: int
+
+
+@dataclass
+class Invoice:
+    customer: str
+    performances: list[Performance]
+
+
+def load_data() -> tuple[list[Invoice], dict[str, Play]]:
     base_dir = os.path.abspath(os.path.dirname(__file__))
     invoice_path = os.path.join(base_dir, "invoices.json")
     plays_path = os.path.join(base_dir, "plays.json")
 
     with open(invoice_path) as invoice_fp, open(plays_path) as plays_fp:
-        invoice = json.load(invoice_fp)
-        plays = json.load(plays_fp)
+        raw_invoices = json.load(invoice_fp)
+        raw_plays = json.load(plays_fp)
 
-    return invoice, plays
+    invoices = [
+        Invoice(
+            customer=invoice["customer"],
+            performances=[
+                Performance(**performance) for performance in invoice["performances"]
+            ],
+        )
+        for invoice in raw_invoices
+    ]
+    plays = {k: Play(**v) for k, v in raw_plays.items()}
+
+    return invoices, plays
 
 
-def statement(invoice: dict, plays: list[dict]):
+def statement(invoice: Invoice, plays: dict[str, Play]):
     total_amount = 0
     volume_credits = 0
-    result = f"청구 내역 (고객명: {invoice['customer']})\n"
+    result = f"청구 내역 (고객명: {invoice.customer})\n"
     format = "${:.2f}"
 
-    for perf in invoice['performances']:
-        play = plays[perf['playID']]
+    for perf in invoice.performances:
+        play = plays[perf.playID]
         this_amount = 0
 
-        if play['type'] == 'tragedy':
+        if play.type == "tragedy":
             this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == 'comedy':
+            if perf.audience > 30:
+                this_amount += 1000 * (perf.audience - 30)
+        elif play.type == "comedy":
             this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-            this_amount += 300 * perf['audience']
+            if perf.audience > 20:
+                this_amount += 10000 + 500 * (perf.audience - 20)
+            this_amount += 300 * perf.audience
         else:
             raise Exception(f"알 수 없는 장르: {play['type']}")
 
         # 포인트 적립
-        volume_credits += max(perf['audience'] - 30, 0)
-        if play['type'] == 'comedy':
-            volume_credits += perf['audience'] // 5
+        volume_credits += max(perf.audience - 30, 0)
+        if play.type == "comedy":
+            volume_credits += perf.audience // 5
 
         # 청구내역 출력
-        result += f"  {play['name']}: {format.format(this_amount / 100)} ({perf['audience']}석)\n"
+        result += (
+            f"  {play.name}: {format.format(this_amount / 100)} ({perf.audience}석)\n"
+        )
         total_amount += this_amount
 
     result += f"총액: {format.format(total_amount / 100)}\n"
--- refactoring_2e/chap01_example/test_statement.py
+++ refactoring_2e/chap01_example/test_statement.py
@@ -1,19 +1,20 @@
 import pytest
-from main import statement
+from main import load_data, statement, Invoice, Performance, Play
 
 
 @pytest.fixture
 def plays():
-    return {
+    data = {
         "hamlet": {"name": "Hamlet", "type": "tragedy"},
         "as-like": {"name": "As You Like It", "type": "comedy"},
         "othello": {"name": "Othello", "type": "tragedy"},
     }
+    return {k: Play(**v) for k, v in data.items()}
 
 
 @pytest.fixture
 def invoice():
-    return {
+    data = {
         "customer": "BigCo",
         "performances": [
             {"playID": "hamlet", "audience": 55},
@@ -21,13 +22,26 @@ def invoice():
             {"playID": "othello", "audience": 40},
         ],
     }
+    return Invoice(
+        customer=data["customer"],
+        performances=[
+            Performance(**performance) for performance in data["performances"]
+        ],
+    )
+
+
+def test_load_data(invoice, plays):
+    i, p = load_data()
+
+    assert i == [invoice]
+    assert p == plays
 
 
 def test_statement(invoice, plays):
     result = statement(invoice, plays)
 
     assert result == (
-"""청구 내역 (고객명: BigCo)
+        """청구 내역 (고객명: BigCo)
   Hamlet: $650.00 (55석)
   As You Like It: $580.00 (35석)
   Othello: $500.00 (40석)
