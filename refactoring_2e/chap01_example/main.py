dfcd6267b195b5d9c38adf5d318684ca974fb015
jayone <imdff0803@gmail.com>
2022-03-04 09:16:12 +0900

    statement - extract func 'apple_sauce'

--- refactoring_2e/chap01_example/main.py
+++ refactoring_2e/chap01_example/main.py
@@ -79,6 +79,14 @@ def statement(invoice: Invoice, plays: dict[str, Play]):
         f = "${:.2f}"
         return f.format(number / 100)
 
+    def apple_sauce():
+        total_amount = 0
+
+        for perf in invoice.performances:
+            total_amount += amount_for(perf)
+
+        return total_amount
+
     def total_volume_credits():
         volume_credits = 0
         for perf in invoice.performances:
@@ -87,7 +95,6 @@ def statement(invoice: Invoice, plays: dict[str, Play]):
         return volume_credits
 
 
-    total_amount = 0
     result = f"청구 내역 (고객명: {invoice.customer})\n"
 
     for perf in invoice.performances:
@@ -95,7 +102,7 @@ def statement(invoice: Invoice, plays: dict[str, Play]):
         result += (
             f"  {play_for(perf).name}: {usd(amount_for(perf))} ({perf.audience}석)\n"
         )
-        total_amount += amount_for(perf)
+    total_amount = apple_sauce()
 
     result += f"총액: {usd(total_amount)}\n"
     result += f"적립 포인트: {total_volume_credits()}\n"
