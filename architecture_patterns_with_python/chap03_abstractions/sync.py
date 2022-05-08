628b3c3697de9f0fbcc070a72669fc7962064eeb
jayone <imdff0803@gmail.com>
2022-05-09 06:29:17 +0900

    architecture_patterns_with_python - chap03 abstractions - version2
    - implement abstraction

--- architecture_patterns_with_python/chap03_abstractions/sync.py
+++ architecture_patterns_with_python/chap03_abstractions/sync.py
@@ -4,6 +4,24 @@ import shutil
 from pathlib import Path
 
 
+def sync(source, dest):
+    # imperative shell step 1, gather inputs
+    source_hashes = read_paths_and_hashes(source)
+    dest_hashes = read_paths_and_hashes(dest)
+
+    # step 2: call functional core
+    actions = determine_actions(source_hashes, dest_hashes, source, dest)
+
+    # imperative shell step 3, apply outputs
+    for action, *paths in actions:
+        if action == "COPY":
+            shutil.copyfile(*paths)
+        if action == "MOVE":
+            shutil.move(*paths)
+        if action == "DELETE":
+            os.remove(paths[0])
+
+
 BLOCKSIZE = 65536
 
 
@@ -17,32 +35,25 @@ def hash_file(path):
     return hasher.hexdigest()
 
 
-def sync(source, dest):
-    # 원본 폴더의 자식들을 순회하면서 파일 이름과 해시의 사전을 만든다.
-    source_hashes = {}
-    for folder, _, files in os.walk(source):
+def read_paths_and_hashes(root):
+    hashes = {}
+    for folder, _, files in os.walk(root):
         for fn in files:
-            source_hashes[hash_file(Path(folder) / fn)] = fn
+            hashes[hash_file(Path(folder) / fn)] = fn
+    return hashes
 
-    seen = set()  # 사본 폴더에서 찾은 파일을 추적한다
 
-    # 사본 폴더 자식들을 순회하면서 파일 이름과 해시를 얻는다.
-    for folder, _, files in os.walk(dest):
-        for fn in files:
-            dest_path = Path(folder) / fn
-            dest_hash = hash_file(dest_path)
-            seen.add(dest_hash)
+def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
+    for sha, filename in source_hashes.items():
+        if sha not in dest_hashes:
+            sourcepath = Path(source_folder) / filename
+            destpath = Path(dest_folder) / filename
+            yield "COPY", sourcepath, destpath
+        elif dest_hashes[sha] != filename:
+            olddestpath = Path(dest_folder) / dest_hashes[sha]
+            newdestpath = Path(dest_folder) / filename
+            yield "MOVE", olddestpath, newdestpath
 
-            # 사본에는 있지만 원본에 없는 파일을 찾으면 삭제한다.
-            if dest_hash not in source_hashes:
-                dest_path.remove()
-
-            # 사본에 있는 파일이 원본과 다른 이름이라면
-            # 사본 이름을 올바른 이름(원본 이름)으로 바꾼다.
-            elif dest_hash in source_hashes and fn != source_hashes[dest_hash]:
-                shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
-
-        # 원본에는 있지만 사본에 없는 모든 파일을 사본으로 복사한다.
-        for src_hash, fn in source_hashes.items():
-            if src_hash not in seen:
-                shutil.copy(Path(source) / fn, Path(dest) / fn)
+    for sha, filename in dest_hashes.items():
+        if sha not in source_hashes:
+            yield "DELETE", dest_folder / filename
