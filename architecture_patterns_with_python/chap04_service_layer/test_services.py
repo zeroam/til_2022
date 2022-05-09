362631ffc8d5cbce01ba99b4f5623ab67e9c553d
jayone <imdff0803@gmail.com>
2022-05-09 18:57:06 +0900

    apply FakeRepository

---
+++ architecture_patterns_with_python/chap04_service_layer/test_services.py
@@ -0,0 +1,44 @@
+import pytest
+import model
+import repository
+import services
+
+
+class FakeRepository(repository.AbstractRepository):
+    def __init__(self, batches) -> None:
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
+
+
+class FakeSession:
+    committed = False
+
+    def commit(self):
+        self.committed = True
+
+
+def test_returns_allocation():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREASKU", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())
+
+
+def test_commits():
+    line = model.OrderLine("o1", "OMNIOUS-MIRROR", 10)
+    batch = model.Batch("b1", "OMNIOUS-MIRROR", 100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True
