16f69538938a4cc27ae25ffc00d0f316124ad830
Dante Jeon <dante.jeon@buzzvil.com>
2022-10-25 20:24:23 +0900

    drf_tutorial: 1. Serialization

--- django/drf_tutorial/tutorial/urls.py
+++ django/drf_tutorial/tutorial/urls.py
@@ -17,11 +17,14 @@ from django.urls import include, path
 from rest_framework import routers
 from tutorial.quickstart import views
 
+import snippets.urls
+
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(snippets.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
